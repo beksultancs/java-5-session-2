0ec8a0669062fb3cdf622b13fd3bac473cf16815
beksultan <selflearningjava@gmail.com>
2022-04-08 08:54:56 +0600

    added something to main method

--- src/peaksoft/Main.java
+++ src/peaksoft/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println("""
                 this is git & github
+                i'm on test branch
                 """);
     }
 }

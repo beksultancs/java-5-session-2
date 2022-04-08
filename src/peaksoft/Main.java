9d3640ed5f9d9ad3097089ea16055157b01dc30f
beksultan <selflearningjava@gmail.com>
2022-04-08 08:50:10 +0600

    update java-5-session-2 1

--- src/peaksoft/Main.java
+++ src/peaksoft/Main.java
@@ -1,9 +1,5 @@
 package peaksoft;
 
-import peaksoft.repositories.DriverRepository;
-import peaksoft.services.DriverService;
-import peaksoft.services.TruckService;
-
 import java.io.FileNotFoundException;
 
 /**
@@ -11,10 +7,8 @@ import java.io.FileNotFoundException;
  */
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        final DriverService driverService = new DriverService();
-        final TruckService truckService = new TruckService();
-
-        driverService.findAllDrivers().forEach(System.out::println);
-        truckService.findAllTrucks().forEach(System.out::println);
+        System.out.println("""
+                this is git & github
+                """);
     }
 }
